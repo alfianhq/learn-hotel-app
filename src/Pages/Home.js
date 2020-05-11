62b9bf22229022d8b042a22744c633f3a3db7fbe
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-11 16:33:10 +0700

    Servies

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Hero from '../Components/Hero';
 import Banner from '../Components/Banner';
 import {Link} from 'react-router-dom';
+import Services from '../Components/Services';
 
 export default function Home() {
 	return (
-		<Hero>
-			<Banner title='luxurios rooms' subtitle='deluxe rooms starting at $299'>
-				<Link to='/rooms' className='btn-primary'>
-					Our Romms
-				</Link>
-			</Banner>
-		</Hero>
+		<>
+			<Hero>
+				<Banner title='luxurios rooms' subtitle='deluxe rooms starting at $299'>
+					<Link to='/rooms' className='btn-primary'>
+						Our Romms
+					</Link>
+				</Banner>
+			</Hero>
+			<Services />
+		</>
 	);
 }
