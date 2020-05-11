e8909d1a1528f1702c05e641224c2b77a8098314
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-11 14:10:46 +0700

    Page Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
 
+// pages
+import Home from './Pages/Home';
+import Rooms from './Pages/Rooms';
+import SingleRoom from './Pages/singleRoom';
+import Error from './Pages/Error';
+
 function App() {
-	return <>Helo form app</>;
+	return (
+		<>
+			<Home />
+			<Rooms />
+			<SingleRoom />
+			<Error />
+		</>
+	);
 }
 
 export default App;
