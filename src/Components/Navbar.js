6ff72eefb3bc727ebf75362c26d3d97f684469dd
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-11 14:54:01 +0700

    button and toggle

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,6 +19,12 @@ export default class Navbar extends Component {
 						<Link to='/'>
 							<img src={logo} alt='logo' />
 						</Link>
+						<button
+							type='button'
+							className='nav-btn'
+							onClick={this.handleToggle}>
+							<FaAlignRight className='nav-icon' />
+						</button>
 					</div>
 				</div>
 			</nav>
