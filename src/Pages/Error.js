b3beb46db65060509dbbed83be6dfe48c591963b
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-11 16:06:34 +0700

    Banner Complete

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Hero from '../Components/Hero';
+import Banner from '../Components/Banner';
+import {Link} from 'react-router-dom';
 
 export default function Error() {
 	return (
-		<div>
-			<h1>Hello from error page</h1>
-		</div>
+		<Hero>
+			<Banner title='404' subtitle='page not found'>
+				<Link to='/' className='btn-primary'>
+					Return home
+				</Link>
+			</Banner>
+		</Hero>
 	);
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import Hero from '../Components/Hero';
+import Banner from '../Components/Banner';
+import {Link} from 'react-router-dom';
 
 export default function Home() {
-	return <Hero></Hero>;
+	return (
+		<Hero>
+			<Banner title='luxurios rooms' subtitle='deluxe rooms starting at $299'>
+				<Link to='/rooms' className='btn-primary'>
+					Our Romms
+				</Link>
+			</Banner>
+		</Hero>
+	);
 }
