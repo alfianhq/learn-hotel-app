1147e3b3263f861b9bee62449e5683faf9d6586e
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-12 18:57:41 +0700

    Type Filter

--- src/Components/RoomsFilter.js
+++ src/Components/RoomsFilter.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 import {useContext} from 'react';
-import {RoomContext} from '../context';
+import {RoomContext} from '../Context';
 import Title from '../Components/Title';
 
 export default function RoomsFilter() {
+	const context = useContext(RoomContext);
+	const {
+		handleChange,
+		type,
+		capacity,
+		price,
+		minPrice,
+		maxPrice,
+		minSize,
+		maxSize,
+		breakfast,
+		pets,
+	} = context;
+
 	return (
-		<div>
-			<h1>Hello From Rooms Filter</h1>
-		</div>
+		<section className='filter-container'>
+			<Title title='search rooms' />
+			<form className='filter-form'>
+				{/* select type */}
+				<div className='form-group'>
+					<label htmlFor='type'>room type</label>
+					<select
+						name='type'
+						id='type'
+						value={type}
+						className='form-control'
+						onChange={handleChange}>
+						<option value='single'>Single</option>
+					</select>
+				</div>
+
+				{/* end selet type */}
+			</form>
+		</section>
 	);
 }
