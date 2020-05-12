16932b5981426ae741f193ae5420fe01b7df3b3b
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-12 18:39:39 +0700

    Type Filter 1

--- src/Components/RoomsFilter.js
+++ src/Components/RoomsFilter.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import {useContext} from 'react';
+import {RoomContext} from '../context';
+import Title from '../Components/Title';
 
-export default class RoomsFilter extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello From Rooms Filter</h1>
-			</div>
-		);
-	}
+export default function RoomsFilter() {
+	return (
+		<div>
+			<h1>Hello From Rooms Filter</h1>
+		</div>
+	);
 }
