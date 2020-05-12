115d84be2d0b878fe65f4f5525567f525660ba7b
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-12 12:17:20 +0700

    function single room

--- src/Pages/singleRoom.js
+++ src/Pages/singleRoom.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../Components/Hero';
+import Banner from '../Components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../Context';
 
-export default function singleRoom() {
-	return (
-		<div>
-			<h1>Hello from single room page</h1>
-		</div>
-	);
+export default class SingleRoom extends Component {
+	constructor(props) {
+		super(props);
+		console.log(this.props);
+		this.state = {
+			slug: this.props.match.params.slug,
+			defaultBcg,
+		};
+	}
+	static contextType = RoomContext;
+	// componentDidMount() {}
+
+	render() {
+		const {getRoom} = this.context;
+		const room = getRoom(this.state.slug);
+		console.log(room);
+
+		return (
+			<div>
+				<h1>Hello from single room page bro</h1>
+			</div>
+		);
+	}
 }
