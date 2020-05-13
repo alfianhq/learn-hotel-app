f533b4675dd796558cfcbfa538dc105f9149505f
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-13 09:29:58 +0700

    Capacity / guest Filter

--- src/Components/RoomsFilter.js
+++ src/Components/RoomsFilter.js
@@ -36,6 +36,16 @@ export default function RoomsFilter({rooms}) {
 			</option>
 		);
 	});
+
+	// GUest function
+	let people = getUnique(rooms, 'capacity');
+	people = people.map((item, index) => {
+		return (
+			<option key={index} value={item}>
+				{item}
+			</option>
+		);
+	});
 	return (
 		<section className='filter-container'>
 			<Title title='search rooms' />
@@ -52,8 +62,21 @@ export default function RoomsFilter({rooms}) {
 						{types}
 					</select>
 				</div>
-
 				{/* end selet type */}
+
+				{/*guests  */}
+				<div className='form-group'>
+					<label htmlFor='capacity'>Guests</label>
+					<select
+						name='capacity'
+						id='capacity'
+						value={capacity}
+						className='form-control'
+						onChange={handleChange}>
+						{people}
+					</select>
+				</div>
+				{/* end guests  */}
 			</form>
 		</section>
 	);
