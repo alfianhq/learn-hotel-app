ed0b54e48de6a691ba72de98532bc9bb9c41acdb
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-13 09:42:27 +0700

    Capacity / guest Filter

--- src/Context.js
+++ src/Context.js
@@ -81,13 +81,20 @@ class RoomProvider extends Component {
 			pets,
 		} = this.state;
 
+		// all the rooms
 		let tempRooms = [...rooms];
 
-		// logic
+		// transfor value
+		capacity = parseInt(capacity);
+
+		// filter by type
 		if (type !== 'all') {
 			tempRooms = tempRooms.filter(room => room.type === type);
 		}
-
+		// filter by capacity
+		if (capacity !== 1) {
+			tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+		}
 		this.setState({
 			sortedRooms: tempRooms,
 		});
